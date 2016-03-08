bed6d2abadc123acccbdd238a034b7c15a7315a0
Kevin Krumwiede <kjkrum@gmail.com>
2016-03-08 16:17:49 -0700

    Moved some doc comments to the interface.

--- src/main/java/com/chalcodes/event/EventBus.java
+++ src/main/java/com/chalcodes/event/EventBus.java
@@ -1,17 +1,47 @@
 package com.chalcodes.event;
 
 /**
- * A type-safe event bus.
+ * A type-safe event bus.  Only {@link #broadcast(Object)} is required to be
+ * thread-safe.
  *
  * @param <T> the event type
  * @author Kevin Krumwiede
  */
 public interface EventBus<T> {
-	EventBus<Exception> getExceptionBus();
-
+	/**
+	 * Registers a receiver.  May throw {@link NullPointerException} if {@code
+	 * receiver} is null.
+	 *
+	 * @param receiver the receiver to register
+	 * @return true if the receiver was registered; otherwise false
+	 */
 	boolean register(EventReceiver<T> receiver);
 
-	void unregister(EventReceiver<T> receiver);
+	/**
+	 * Unregisters a receiver.  The receiver is guaranteed not to receive any
+	 * further callbacks until it is registered again.  May throw {@link
+	 * NullPointerException} if {@code receiver} is null.
+	 *
+	 * @param receiver the receiver to unregister
+	 * @return true if the receiver was unregistered; otherwise false
+	 */
+	boolean unregister(EventReceiver<T> receiver);
 
+	/**
+	 * Broadcasts an event to receivers that are registered at the time this
+	 * method is called, excluding any that are unregistered before the
+	 * dispatch occurs.  May throw {@link NullPointerException} if {@code
+	 * event} is null and the implementation does not allow null events.
+	 *
+	 * @param event the event to broadcast
+	 */
 	void broadcast(T event);
+
+	/**
+	 * Gets the bus on which this bus broadcasts exceptions thrown by its
+	 * receivers.
+	 *
+	 * @return the exception bus; may be null
+	 */
+	EventBus<Exception> getExceptionBus();
 }
