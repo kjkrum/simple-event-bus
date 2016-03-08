c09090f0929ee973f2043f27169dad063d8e8214
Kevin Krumwiede <kjkrum@gmail.com>
2016-03-08 16:19:57 -0700

    0.6.1: SimpleEventBus manages the copy-on-write behavior of its listener collection.

--- src/main/java/com/chalcodes/event/SimpleEventBus.java
+++ src/main/java/com/chalcodes/event/SimpleEventBus.java
@@ -1,12 +1,14 @@
 package com.chalcodes.event;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Executor;
 
 /**
- * A type-safe event bus.
+ * An event bus that dispatches events using an {@link Executor}.  The
+ * executor should be single-threaded, and all methods except {@link
+ * #broadcast(Object)} should be called in the executor thread.
  *
  * @param <T> the event type
  * @author Kevin Krumwiede
@@ -15,18 +17,17 @@ public class SimpleEventBus<T> implements EventBus<T> {
 	protected final Executor mExecutor;
 	private final EventBus<Exception> mExceptionBus;
 	private final boolean mNullAllowed;
-	private final Set<EventReceiver<T>> mReceivers = new CopyOnWriteArraySet<EventReceiver<T>>();
+	/** Copy-on-write. */
+	private volatile Set<EventReceiver<T>> mReceivers;
 
 	/**
-	 * Creates a new event bus.  The executor should be single-threaded, and
-	 * all methods except {@link #broadcast(Object)} should be called in the
-	 * executor thread.  If an exception bus is provided, any exception thrown
-	 * by a receiver will be broadcast on it.
+	 * Creates a new event bus.  If an exception bus is provided, any
+	 * exception thrown by a receiver will be broadcast on it.
 	 *
 	 * @param executor     the broadcast executor
 	 * @param exceptionBus the exception bus; may be null
-	 * @param nullAllowed  true if this bus should allow null events; otherwise
-	 *                     false
+	 * @param nullAllowed  true if this bus should allow null events;
+	 *                     otherwise false
 	 * @throws NullPointerException if executor is null
 	 */
 	public SimpleEventBus(final Executor executor, final EventBus<Exception> exceptionBus,
@@ -39,41 +40,53 @@ public class SimpleEventBus<T> implements EventBus<T> {
 		mNullAllowed = nullAllowed;
 	}
 
-	/**
-	 * Gets the exception bus for this bus.
-	 *
-	 * @return the exception bus
-	 */
 	@Override
 	public EventBus<Exception> getExceptionBus() {
 		return mExceptionBus;
 	}
 
-	/**
-	 * Immediately registers a receiver.
-	 *
-	 * @param receiver the receiver to register
-	 */
 	@Override
 	public boolean register(final EventReceiver<T> receiver) {
 		if(receiver == null) {
 			throw new NullPointerException();
 		}
-		return mReceivers.add(receiver);
+		if(mReceivers == null) {
+			final Set<EventReceiver<T>> tmp = new HashSet<EventReceiver<T>>();
+			tmp.add(receiver);
+			mReceivers = tmp;
+			return true;
+		}
+		else if(mReceivers.contains(receiver)) {
+			return false;
+		}
+		else {
+			final Set<EventReceiver<T>> tmp = new HashSet<EventReceiver<T>>(mReceivers);
+			tmp.add(receiver);
+			mReceivers = tmp;
+			return true;
+		}
 	}
 
-	/**
-	 * Immediately unregisters a receiver.  The receiver is guaranteed not to
-	 * receive any pending event dispatch.
-	 *
-	 * @param receiver the receiver to unregister
-	 */
 	@Override
-	public void unregister(final EventReceiver<T> receiver) {
+	public boolean unregister(final EventReceiver<T> receiver) {
 		if(receiver == null) {
 			throw new NullPointerException();
 		}
-		mReceivers.remove(receiver);
+		if(mReceivers != null && mReceivers.contains(receiver)) {
+			if(mReceivers.size() == 1) {
+				mReceivers = null;
+				return true;
+			}
+			else {
+				final Set<EventReceiver<T>> tmp = new HashSet<EventReceiver<T>>(mReceivers);
+				tmp.remove(receiver);
+				mReceivers = tmp;
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
 	}
 
 	/**
@@ -88,29 +101,38 @@ public class SimpleEventBus<T> implements EventBus<T> {
 		if(event == null && !mNullAllowed) {
 			throw new NullPointerException();
 		}
-		mExecutor.execute(new Runnable() {
-			final Iterator<EventReceiver<T>> iter = mReceivers.iterator();
+		final Set<EventReceiver<T>> snapshot = mReceivers;
+		if(snapshot != null) {
+			mExecutor.execute(new Runnable() {
+				final Iterator<EventReceiver<T>> iter = snapshot.iterator();
 
-			@Override
-			public void run() {
-				while(iter.hasNext()) {
-					dispatch(iter.next(), event);
+				@Override
+				public void run() {
+					while(iter.hasNext()) {
+						dispatch(iter.next(), event);
+					}
 				}
-			}
-		});
+			});
+		}
 	}
 
+	/**
+	 * Dispatches an event only if the receiver is registered.  Unregisters
+	 * the receiver if it throws an exception.
+	 *
+	 * @param receiver the receiver
+	 * @param event the event to dispatch
+	 */
 	protected void dispatch(final EventReceiver<T> receiver, final T event) {
-		if(mReceivers.contains(receiver)) {
+		if(mReceivers != null && mReceivers.contains(receiver)) {
 			try {
 				receiver.onEvent(this, event);
 			} catch(Exception e) {
-				mReceivers.remove(receiver);
+				unregister(receiver);
 				if(mExceptionBus != null) {
 					mExceptionBus.broadcast(e);
 				}
 			}
 		}
 	}
-
 }
