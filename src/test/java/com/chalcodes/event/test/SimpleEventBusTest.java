5204837c44c388c57d1790b0a5bd5604af85c385
kjkrum <kjkrum@gmail.com>
2017-03-14 02:04:05 -0700

    Unit tests obsolete.  Will rewrite.

--- src/test/java/com/chalcodes/event/test/SimpleEventBusTest.java
+++ src/test/java/com/chalcodes/event/test/SimpleEventBusTest.java
@@ -1,20 +1,6 @@
 package com.chalcodes.event.test;
 
-import com.chalcodes.event.EventBus;
-import com.chalcodes.event.EventReceiver;
 import com.chalcodes.event.SimpleEventBus;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import javax.annotation.Nonnull;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Tests for {@link SimpleEventBus}.
@@ -22,175 +8,175 @@ import static org.junit.Assert.fail;
  * @author Kevin Krumwiede
  */
 public class SimpleEventBusTest {
-	private static ExecutorService gExecutor;
-
-	@BeforeClass
-	public static void setup() {
-		gExecutor = Executors.newFixedThreadPool(1);
-	}
-
-	@AfterClass
-	public static void teardown() {
-		if(gExecutor != null) {
-			gExecutor.shutdown();
-		}
-	}
-
-	private volatile boolean mEventReceived;
-
-	/**
-	 * A registered receiver receives an event.
-	 */
-	@Test
-	public void registeredReceiver() {
-		final CountDownLatch latch = new CountDownLatch(1);
-		gExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				final String testEvent = "test";
-				final EventReceiver<String> receiver = new EventReceiver<String>() {
-					@Override
-					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
-						if(event.equals(testEvent)) {
-							mEventReceived = true;
-						}
-					}
-				};
-				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, null);
-				bus.register(receiver);
-				bus.broadcast(testEvent);
-				gExecutor.execute(new Runnable() {
-					@Override
-					public void run() {
-						latch.countDown();
-					}
-				});
-			}
-		});
-		try {
-			latch.await();
-		}
-		catch(InterruptedException e) {
-			fail("interrupted");
-		}
-		assertTrue(mEventReceived);
-	}
-
-	/**
-	 * A unregistered receiver does not receive an event.
-	 */
-	@Test
-	public void unregisteredReceiver() {
-		final CountDownLatch latch = new CountDownLatch(1);
-		gExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				final String testEvent = "test";
-				final EventReceiver<String> receiver = new EventReceiver<String>() {
-					@Override
-					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
-						if(event.equals(testEvent)) {
-							mEventReceived = true;
-						}
-					}
-				};
-				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, null);
-				bus.register(receiver);
-				bus.broadcast(testEvent);
-				bus.unregister(receiver);
-				gExecutor.execute(new Runnable() {
-					@Override
-					public void run() {
-						latch.countDown();
-					}
-				});
-			}
-		});
-		try {
-			latch.await();
-		}
-		catch(InterruptedException e) {
-			fail("interrupted");
-		}
-		assertFalse(mEventReceived);
-	}
-
-	private volatile boolean mExceptionReceived;
-
-	/**
-	 * A receiver that throws an exception is removed, and the exception is
-	 * broadcast on the exception bus.
-	 */
-	@Test
-	public void badReceiver() {
-		final CountDownLatch latch = new CountDownLatch(1);
-		gExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				final String testEvent = "test";
-				final EventBus<Exception> exceptionBus = new SimpleEventBus<Exception>(gExecutor, null);
-				exceptionBus.register(new EventReceiver<Exception>() {
-					@Override
-					public void onEvent(@Nonnull final EventBus<Exception> bus, @Nonnull final Exception event) {
-						mExceptionReceived = true;
-					}
-				});
-				final EventReceiver<String> receiver = new EventReceiver<String>() {
-					private boolean calledBefore;
-
-					@Override
-					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
-						if(!calledBefore) {
-							calledBefore = true;
-							throw new RuntimeException();
-						}
-						mEventReceived = true;
-					}
-				};
-				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, exceptionBus);
-				bus.register(receiver);
-				bus.broadcast(testEvent);
-				bus.broadcast(testEvent);
-				gExecutor.execute(new Runnable() {
-					@Override
-					public void run() {
-						gExecutor.execute(new Runnable() {
-							@Override
-							public void run() {
-								latch.countDown();
-							}
-						});
-					}
-				});
-			}
-		});
-		try {
-			latch.await();
-		}
-		catch(InterruptedException e) {
-			fail("interrupted");
-		}
-		assertFalse(mEventReceived);
-		assertTrue(mExceptionReceived);
-	}
-
-	/**
-	 * Registering a null receiver throws a {@code NullPointerException}.
-	 */
-	@Test(expected = NullPointerException.class)
-	public void nullReceiver() {
-		final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, null);
-		//noinspection ConstantConditions
-		bus.register(null);
-	}
-
-	/**
-	 * Broadcasting a null event throws a {@code NullPointerException}.
-	 */
-	@Test(expected = NullPointerException.class)
-	public void nullEvent() {
-		final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, null);
-		//noinspection ConstantConditions
-		bus.broadcast(null);
-	}
+//	private static ExecutorService gExecutor;
+//
+//	@BeforeClass
+//	public static void setup() {
+//		gExecutor = Executors.newFixedThreadPool(1);
+//	}
+//
+//	@AfterClass
+//	public static void teardown() {
+//		if(gExecutor != null) {
+//			gExecutor.shutdown();
+//		}
+//	}
+//
+//	private volatile boolean mEventReceived;
+//
+//	/**
+//	 * A registered receiver receives an event.
+//	 */
+//	@Test
+//	public void registeredReceiver() {
+//		final CountDownLatch latch = new CountDownLatch(1);
+//		gExecutor.execute(new Runnable() {
+//			@Override
+//			public void run() {
+//				final String testEvent = "test";
+//				final EventReceiver<String> receiver = new EventReceiver<String>() {
+//					@Override
+//					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
+//						if(event.equals(testEvent)) {
+//							mEventReceived = true;
+//						}
+//					}
+//				};
+//				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor);
+//				bus.register(receiver);
+//				bus.broadcast(testEvent);
+//				gExecutor.execute(new Runnable() {
+//					@Override
+//					public void run() {
+//						latch.countDown();
+//					}
+//				});
+//			}
+//		});
+//		try {
+//			latch.await();
+//		}
+//		catch(InterruptedException e) {
+//			fail("interrupted");
+//		}
+//		assertTrue(mEventReceived);
+//	}
+//
+//	/**
+//	 * A unregistered receiver does not receive an event.
+//	 */
+//	@Test
+//	public void unregisteredReceiver() {
+//		final CountDownLatch latch = new CountDownLatch(1);
+//		gExecutor.execute(new Runnable() {
+//			@Override
+//			public void run() {
+//				final String testEvent = "test";
+//				final EventReceiver<String> receiver = new EventReceiver<String>() {
+//					@Override
+//					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
+//						if(event.equals(testEvent)) {
+//							mEventReceived = true;
+//						}
+//					}
+//				};
+//				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor);
+//				bus.register(receiver);
+//				bus.broadcast(testEvent);
+//				bus.unregister(receiver);
+//				gExecutor.execute(new Runnable() {
+//					@Override
+//					public void run() {
+//						latch.countDown();
+//					}
+//				});
+//			}
+//		});
+//		try {
+//			latch.await();
+//		}
+//		catch(InterruptedException e) {
+//			fail("interrupted");
+//		}
+//		assertFalse(mEventReceived);
+//	}
+//
+//	private volatile boolean mExceptionReceived;
+//
+//	/**
+//	 * A receiver that throws an exception is removed, and the exception is
+//	 * broadcast on the exception bus.
+//	 */
+//	@Test
+//	public void badReceiver() {
+//		final CountDownLatch latch = new CountDownLatch(1);
+//		gExecutor.execute(new Runnable() {
+//			@Override
+//			public void run() {
+//				final String testEvent = "test";
+//				final EventBus<Exception> exceptionBus = new SimpleEventBus<Exception>(gExecutor);
+//				exceptionBus.register(new EventReceiver<Exception>() {
+//					@Override
+//					public void onEvent(@Nonnull final EventBus<Exception> bus, @Nonnull final Exception event) {
+//						mExceptionReceived = true;
+//					}
+//				});
+//				final EventReceiver<String> receiver = new EventReceiver<String>() {
+//					private boolean calledBefore;
+//
+//					@Override
+//					public void onEvent(@Nonnull final EventBus<String> bus, @Nonnull final String event) {
+//						if(!calledBefore) {
+//							calledBefore = true;
+//							throw new RuntimeException();
+//						}
+//						mEventReceived = true;
+//					}
+//				};
+//				final EventBus<String> bus = new SimpleEventBus<String>(gExecutor, exceptionBus);
+//				bus.register(receiver);
+//				bus.broadcast(testEvent);
+//				bus.broadcast(testEvent);
+//				gExecutor.execute(new Runnable() {
+//					@Override
+//					public void run() {
+//						gExecutor.execute(new Runnable() {
+//							@Override
+//							public void run() {
+//								latch.countDown();
+//							}
+//						});
+//					}
+//				});
+//			}
+//		});
+//		try {
+//			latch.await();
+//		}
+//		catch(InterruptedException e) {
+//			fail("interrupted");
+//		}
+//		assertFalse(mEventReceived);
+//		assertTrue(mExceptionReceived);
+//	}
+//
+//	/**
+//	 * Registering a null receiver throws a {@code NullPointerException}.
+//	 */
+//	@Test(expected = NullPointerException.class)
+//	public void nullReceiver() {
+//		final EventBus<String> bus = new SimpleEventBus<String>(gExecutor);
+//		//noinspection ConstantConditions
+//		bus.register(null);
+//	}
+//
+//	/**
+//	 * Broadcasting a null event throws a {@code NullPointerException}.
+//	 */
+//	@Test(expected = NullPointerException.class)
+//	public void nullEvent() {
+//		final EventBus<String> bus = new SimpleEventBus<String>(gExecutor);
+//		//noinspection ConstantConditions
+//		bus.broadcast(null);
+//	}
 }
--- src/test/java/com/chalcodes/event/test/StickyEventBusTest.java
+++ src/test/java/com/chalcodes/event/test/StickyEventBusTest.java
@@ -1,20 +1,6 @@
 package com.chalcodes.event.test;
 
-import com.chalcodes.event.EventBus;
-import com.chalcodes.event.EventReceiver;
 import com.chalcodes.event.StickyEventBus;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import javax.annotation.Nonnull;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Tests for {@link StickyEventBus}.
@@ -22,152 +8,47 @@ import static org.junit.Assert.fail;
  * @author Kevin Krumwiede
  */
 public class StickyEventBusTest {
-	private static ExecutorService gExecutor;
-
-	@BeforeClass
-	public static void setup() {
-		gExecutor = Executors.newFixedThreadPool(1);
-	}
-
-	@AfterClass
-	public static void teardown() {
-		if(gExecutor != null) {
-			gExecutor.shutdown();
-		}
-	}
-
-	private volatile boolean mEventReceived;
-
-	/**
-	 * A receiver registered after a broadcast receives the event.
-	 */
-	@Test
-	public void registerAfterBroadcast() {
-		final CountDownLatch latch = new CountDownLatch(1);
-		gExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				final String testEvent = "test";
-				final EventReceiver<String> receiver = new EventReceiver<String>() {
-					@Override
-					public void onEvent(@Nonnull EventBus<String> bus, @Nonnull String event) {
-						if(event.equals(testEvent)) {
-							mEventReceived = true;
-						}
-					}
-				};
-				final EventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
-				bus.broadcast(testEvent);
-				bus.register(receiver);
-				gExecutor.execute(new Runnable() {
-					@Override
-					public void run() {
-						latch.countDown();
-					}
-				});
-			}
-		});
-		try {
-			latch.await();
-		} catch(InterruptedException e) {
-			fail("interrupted");
-		}
-		assertTrue(mEventReceived);
-	}
-
-	/**
-	 * A receiver registered and unregistered after a broadcast does not
-	 * receive an event.
-	 */
-	@Test
-	public void registerAndUnregisterAfterBroadcast() {
-		final CountDownLatch latch = new CountDownLatch(1);
-		gExecutor.execute(new Runnable() {
-			@Override
-			public void run() {
-				final String testEvent = "test";
-				final EventReceiver<String> receiver = new EventReceiver<String>() {
-					@Override
-					public void onEvent(@Nonnull EventBus<String> bus, @Nonnull String event) {
-							mEventReceived = true;
-					}
-				};
-				final EventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
-				bus.broadcast(testEvent);
-				bus.register(receiver);
-				bus.unregister(receiver);
-				gExecutor.execute(new Runnable() {
-					@Override
-					public void run() {
-						latch.countDown();
-					}
-				});
-			}
-		});
-		try {
-			latch.await();
-		} catch(InterruptedException e) {
-			fail("interrupted");
-		}
-		assertFalse(mEventReceived);
-	}
-
-
-//	/**
-//	 * An event becomes sticky immediately, even without a receiver.
-//	 */
-//	@Test
-//	public void eventBecomesSticky() {
-//		final CountDownLatch latch = new CountDownLatch(1);
-//		final String testEvent = "test";
-//		gExecutor.execute(new Runnable() {
-//			@Override
-//			public void run() {
-//				final StickyEventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
-//				bus.broadcast(testEvent);
-//				mEventValue = bus.getSticky();
-//				latch.countDown();
-//			}
-//		});
-//		try {
-//			latch.await();
-//		} catch(InterruptedException e) {
-//			fail("interrupted");
-//		}
-//		assertEquals(testEvent, mEventValue);
+//	private static ExecutorService gExecutor;
+//
+//	@BeforeClass
+//	public static void setup() {
+//		gExecutor = Executors.newFixedThreadPool(1);
 //	}
 //
+//	@AfterClass
+//	public static void teardown() {
+//		if(gExecutor != null) {
+//			gExecutor.shutdown();
+//		}
+//	}
+//
+//	private volatile boolean mEventReceived;
+//
 //	/**
-//	 * Tests for regression of <a
-//	 * href="https://github.com/kjkrum/simple-event-bus/issues/1">issue
-//	 * #1</a>.
+//	 * A receiver registered after a broadcast receives the event.
 //	 */
 //	@Test
-//	public void issue1() {
+//	public void registerAfterBroadcast() {
 //		final CountDownLatch latch = new CountDownLatch(1);
-//		final String oldEvent = "foo";
-//		final String newEvent = "bar";
 //		gExecutor.execute(new Runnable() {
 //			@Override
 //			public void run() {
-//				final SimpleEventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
-//				bus.broadcast(oldEvent);
+//				final String testEvent = "test";
+//				final EventReceiver<String> receiver = new EventReceiver<String>() {
+//					@Override
+//					public void onEvent(@Nonnull EventBus<String> bus, @Nonnull String event) {
+//						if(event.equals(testEvent)) {
+//							mEventReceived = true;
+//						}
+//					}
+//				};
+//				final EventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
+//				bus.broadcast(testEvent);
+//				bus.register(receiver);
 //				gExecutor.execute(new Runnable() {
 //					@Override
 //					public void run() {
-//						bus.broadcast(newEvent);
-//						bus.register(new EventReceiver<String>() {
-//							@Override
-//							public void onEvent(@Nonnull EventBus<String> bus, String event) {
-//								mEventValue = event;
-//							}
-//						});
-//						gExecutor.execute(new Runnable() {
-//							@Override
-//							public void run() {
-//								latch.countDown();
-//							}
-//						});
+//						latch.countDown();
 //					}
 //				});
 //			}
@@ -177,6 +58,111 @@ public class StickyEventBusTest {
 //		} catch(InterruptedException e) {
 //			fail("interrupted");
 //		}
-//		assertEquals(newEvent, mEventValue);
+//		assertTrue(mEventReceived);
 //	}
+//
+//	/**
+//	 * A receiver registered and unregistered after a broadcast does not
+//	 * receive an event.
+//	 */
+//	@Test
+//	public void registerAndUnregisterAfterBroadcast() {
+//		final CountDownLatch latch = new CountDownLatch(1);
+//		gExecutor.execute(new Runnable() {
+//			@Override
+//			public void run() {
+//				final String testEvent = "test";
+//				final EventReceiver<String> receiver = new EventReceiver<String>() {
+//					@Override
+//					public void onEvent(@Nonnull EventBus<String> bus, @Nonnull String event) {
+//							mEventReceived = true;
+//					}
+//				};
+//				final EventBus<String> bus = new StickyEventBus<String>(gExecutor);
+//				bus.broadcast(testEvent);
+//				bus.register(receiver);
+//				bus.unregister(receiver);
+//				gExecutor.execute(new Runnable() {
+//					@Override
+//					public void run() {
+//						latch.countDown();
+//					}
+//				});
+//			}
+//		});
+//		try {
+//			latch.await();
+//		} catch(InterruptedException e) {
+//			fail("interrupted");
+//		}
+//		assertFalse(mEventReceived);
+//	}
+//
+//
+////	/**
+////	 * An event becomes sticky immediately, even without a receiver.
+////	 */
+////	@Test
+////	public void eventBecomesSticky() {
+////		final CountDownLatch latch = new CountDownLatch(1);
+////		final String testEvent = "test";
+////		gExecutor.execute(new Runnable() {
+////			@Override
+////			public void run() {
+////				final StickyEventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
+////				bus.broadcast(testEvent);
+////				mEventValue = bus.getSticky();
+////				latch.countDown();
+////			}
+////		});
+////		try {
+////			latch.await();
+////		} catch(InterruptedException e) {
+////			fail("interrupted");
+////		}
+////		assertEquals(testEvent, mEventValue);
+////	}
+////
+////	/**
+////	 * Tests for regression of <a
+////	 * href="https://github.com/kjkrum/simple-event-bus/issues/1">issue
+////	 * #1</a>.
+////	 */
+////	@Test
+////	public void issue1() {
+////		final CountDownLatch latch = new CountDownLatch(1);
+////		final String oldEvent = "foo";
+////		final String newEvent = "bar";
+////		gExecutor.execute(new Runnable() {
+////			@Override
+////			public void run() {
+////				final SimpleEventBus<String> bus = new StickyEventBus<String>(gExecutor, null);
+////				bus.broadcast(oldEvent);
+////				gExecutor.execute(new Runnable() {
+////					@Override
+////					public void run() {
+////						bus.broadcast(newEvent);
+////						bus.register(new EventReceiver<String>() {
+////							@Override
+////							public void onEvent(@Nonnull EventBus<String> bus, String event) {
+////								mEventValue = event;
+////							}
+////						});
+////						gExecutor.execute(new Runnable() {
+////							@Override
+////							public void run() {
+////								latch.countDown();
+////							}
+////						});
+////					}
+////				});
+////			}
+////		});
+////		try {
+////			latch.await();
+////		} catch(InterruptedException e) {
+////			fail("interrupted");
+////		}
+////		assertEquals(newEvent, mEventValue);
+////	}
 }
